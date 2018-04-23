d9170690d4b167b3626f2336f015be454d196a9d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/NetCoreWebApplication/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardWeb;
+using DevExpress.AspNetCore;
+
+namespace NetCoreWebApplication {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            // Add a DashboardController class descendant with a specified dashboard storage 
+            // and a connection string provider. 
+            services
+                .AddMvc()
+                .AddDefaultDashboardController(configurator => {
+                    configurator.SetDashboardStorage(new DashboardFileStorage("App_Data\\Dashboards"));
+                    configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(Configuration));
+                });
+            // Add the third-party (JQuery, Knockout, etc.) and DevExtreme libraries. 
+            services.AddDevExpressControls(settings => settings.Resources = ResourcesType.ThirdParty | ResourcesType.DevExtreme);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            } else {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            // Register the DevExpress middleware.
+            app.UseDevExpressControls();
+            app.UseMvc(routes => {
+                // Map dashboard routes.
+                routes.MapDashboardRoute();
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Getting Started - Create an ASP.NET Core Dashboard Designer (Runtime sample)
+
+
+The example contains the source code of the project created in this step-by-step tutorial: <a href="https://documentation.devexpress.com/Dashboard/119284/Getting-Started/">Creating an ASP.NET Core Dashboard Designer</a>.
+
+<br/>
+
+
